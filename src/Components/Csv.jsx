c18da82dbe7ba07b195df51e8c4b88e225498cd9
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 21:56:14 +0530

    Update Csv.jsx

--- src/Components/Csv.jsx
+++ src/Components/Csv.jsx
@@ -1,27 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import GetAppIcon from "@mui/icons-material/GetApp";
 import { CSVLink } from "react-csv";
 
 export default function Csv(props) {
   const cvsDataFun = async () => {
-    var tempArr = [];
-
     var arr = {};
 
     for (let i = 0; i < props.data.length; i++) {
-      arr[i].name =
+      var tempArr = {};
+
+      tempArr["name"] =
         props.data[i].name.title +
         " " +
         props.data[i].name.first +
         " " +
         props.data[i].name.last;
-      arr[i].dob = props.data[i].dob.date;
-      arr[i].email = props.data[i].email;
-      arr[i].gender = props.data[i].gender;
+      tempArr["dob"] = props.data[i].dob.date;
+      tempArr["email"] = props.data[i].email;
+      tempArr["gender"] = props.data[i].gender;
 
-      //   tempArr = tempArr.concat(arr);
-
-      console.log(arr);
+      arr[i] = tempArr;
     }
 
     console.log(arr);
