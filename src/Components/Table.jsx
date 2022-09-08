cd4f2920aae3d8cf54fd59ded8a66d02c6bde926
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 13:17:42 +0530

    Update Table.jsx

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -4,30 +4,12 @@ import Moment from "react-moment";
 export default function Table(props) {
   const [search, setsearch] = useState("");
   const [showtrue, setTrue] = useState(false);
-  const [sort, setsort] = useState(props.data);
+  // const [sort, setsort] = useState(props.data);
 
   const filterData = async function (e) {
     setsearch(e.target.value);
   };
 
-  const changeSort = async () => {
-    let sortData = props.data;
-    if (showtrue) {
-      const sortVar = [...sort].sort((a, b) =>
-        a.name.first > b.name.first ? 1 : -1
-      );
-      setsort(sortVar);
-    } else {
-      const sortVar = [...sort].sort((a, b) =>
-        a.name.first < b.name.first ? 1 : -1
-      );
-      setsort(sortVar);
-    }
-    // if (e.target.value === "low") {
-    //   const sort = [...showGender].sort((a, b) => (a.price > b.price ? 1 : -1));
-    //   setGender(sort);
-    // console.log(showGender);
-  };
   return (
     <>
       <input
@@ -44,9 +26,9 @@ export default function Table(props) {
           <th>DOB</th>
           <th>Email</th>
         </tr>
-        {sort ? (
+        {props.data ? (
           <>
-            {sort
+            {props.data
               .filter((value) => {
                 let name =
                   value.name.title +
@@ -69,7 +51,7 @@ export default function Table(props) {
               .map((value, key) => {
                 return (
                   <tr key={key}>
-                    <td onClick={changeSort}>
+                    <td onChange={setTrue(!showtrue)}>
                       <span>{value.name.title}</span>{" "}
                       <span>{value.name.first}</span>{" "}
                       <span>{value.name.last}</span>
