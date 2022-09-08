4eacb2d90d55a186b736176ca9dc4bc8a7017961
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 21:58:49 +0530

    Update Csv.jsx

--- src/Components/Csv.jsx
+++ src/Components/Csv.jsx
@@ -3,35 +3,12 @@ import GetAppIcon from "@mui/icons-material/GetApp";
 import { CSVLink } from "react-csv";
 
 export default function Csv(props) {
-  const cvsDataFun = async () => {
-    var arr = {};
-
-    for (let i = 0; i < props.data.length; i++) {
-      var tempArr = {};
-
-      tempArr["name"] =
-        props.data[i].name.title +
-        " " +
-        props.data[i].name.first +
-        " " +
-        props.data[i].name.last;
-      tempArr["dob"] = props.data[i].dob.date;
-      tempArr["email"] = props.data[i].email;
-      tempArr["gender"] = props.data[i].gender;
-
-      arr[i] = tempArr;
-    }
-
-    console.log(arr);
-  };
-
   return (
-    <CSVLink data={props.data} className="linkRemove">
-      <div className="mDivCSV">
-        <GetAppIcon />
-        <p>Export</p>
-      </div>
-      //{" "}
-    </CSVLink>
+    // <CSVLink data={props.data} className="linkRemove">
+    <div className="mDivCSV">
+      <GetAppIcon />
+      <p>Export</p>
+    </div>
+    // </CSVLink>
   );
 }
