6fed7f43a079b43ea516a5075f671d6cf55df9e6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 12:02:01 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
   return <div>Hello World</div>;
 }
