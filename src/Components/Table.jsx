866398a87d55f4ffaffcc8187fa675db99d322af
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 12:32:39 +0530

    Update Table.jsx

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -15,18 +15,23 @@ export default function Table(props) {
         {props.data ? (
           <>
             {props.data.map((value, map) => {
-              return <tr></tr>;
+              return (
+                <tr>
+                  <td>
+                    <span>{value.name.title}</span>{" "}
+                    <span>{value.name.first}</span>{" "}
+                    <span>{value.name.last}</span>
+                  </td>
+                  <td>{value.gender}</td>
+                  <td>{value.dob.date}</td>
+                  <td>{value.email}</td>
+                </tr>
+              );
             })}
           </>
         ) : (
           "No Data"
         )}
-        <tr>
-          <td>Company</td>
-          <td>Contact</td>
-          <td>Country</td>
-          <td>Country</td>
-        </tr>
       </table>
     </>
   );
