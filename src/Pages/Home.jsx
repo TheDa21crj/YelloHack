c7d8f57a92c46105e4be804cb6e31ba8386fdb78
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 13:43:22 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,34 +6,6 @@ export default function Home() {
 
   return (
     <div>
-      {/* {showData ? (
-        <>
-          {showData.map((value, key) => {
-            return (
-              <div>
-                <p>
-                  name = <span>{value.name.title}</span>{" "}
-                  <span>{value.name.first}</span> <span>{value.name.last}</span>
-                </p>
-
-                <p>
-                  Gender: <span>{value.gender}</span>
-                </p>
-                <p>
-                  DOB: <span>{value.dob.date}</span>
-                </p>
-                <p>
-                  Email: <span>{value.email}</span>
-                </p>
-                <br />
-                <br />
-              </div>
-            );
-          })}
-        </>
-      ) : (
-        "No Data"
-      )} */}
       <Table />
     </div>
   );
