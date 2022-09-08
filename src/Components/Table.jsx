cd911c952c3712606566c9a008044fa56bd43aa8
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 20:23:23 +0530

    Update Table.jsx

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Moment from "react-moment";
 import Csv from "./Csv";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Table() {
   const [search, setsearch] = useState("");
