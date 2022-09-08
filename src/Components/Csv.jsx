f4a87a9acd74338a20039912a3a39d8369d89ca4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 21:23:33 +0530

    Update Csv.jsx

--- src/Components/Csv.jsx
+++ src/Components/Csv.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GetAppIcon from "@mui/icons-material/GetApp";
 import { CSVLink } from "react-csv";
 
 export default function Csv(props) {
+  const csvData = [];
   return (
-    <CSVLink data={props.data}>
+    <CSVLink data={props.data} className="linkRemove">
       <div className="mDivCSV">
         <GetAppIcon />
         <p>Export</p>
