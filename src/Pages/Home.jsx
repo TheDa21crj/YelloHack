bc01a01fb3cd4923af346953ceadce78778c7ec1
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 13:43:12 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,22 +4,6 @@ import Table from "./../Components/Table";
 export default function Home() {
   const [showData, setData] = useState([]);
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-
-  // const getData = async () => {
-  //   const response = await fetch("https://randomuser.me/api/?results=10");
-  //   const data = await response.json();
-
-  //   // console.table(data.results);
-  //   if (data.results.length > 0) {
-  //     setData(data.results);
-  //   } else {
-  //     setData([]);
-  //   }
-  // };
-
   return (
     <div>
       {/* {showData ? (
