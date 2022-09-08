4c3f7cde89932110b5749e9a95da59af5a39b3d9
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-09-08 20:26:23 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Table from "./../Components/Table";
 
 export default function Home() {
-  const [showData, setData] = useState([]);
-
   return (
     <div>
       <Table />
